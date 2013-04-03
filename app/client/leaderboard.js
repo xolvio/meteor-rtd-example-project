bf5b61bde7a88f3365b01383b466b6250bf5f22a
sam <sam@hatoum.net>
2013-04-03 12:31:34 -0700

    using strict

--- app/client/leaderboard.js
+++ app/client/leaderboard.js
@@ -1,24 +1,27 @@
-Template.leaderboard.players = function () {
-    return Players.find({}, {sort: {score: -1, name: 1}});
-};
+(function () {
+    "use strict";
+    Template.leaderboard.players = function () {
+        return Players.find({}, {sort: {score: -1, name: 1}});
+    };
 
-Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-};
+    Template.leaderboard.selected_name = function () {
+        var player = Players.findOne(Session.get("selected_player"));
+        return player && player.name;
+    };
 
-Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-};
+    Template.player.selected = function () {
+        return Session.equals("selected_player", this._id) ? "selected" : '';
+    };
 
-Template.leaderboard.events({
-    'click input.inc': function () {
-        Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-});
+    Template.leaderboard.events({
+        'click input.inc': function () {
+            Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+        }
+    });
 
-Template.player.events({
-    'click': function () {
-        Session.set("selected_player", this._id);
-    }
-});
+    Template.player.events({
+        'click': function () {
+            Session.set("selected_player", this._id);
+        }
+    });
+})();
