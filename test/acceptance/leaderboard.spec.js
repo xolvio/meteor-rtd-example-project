8869f518b8e6592bcc76a65ef0166aceb4a2f7bb
Nomeasmo <oliver@corabojahr.de>
2014-02-10 16:02:24 +0100

    Issue 113: - Refactored webdriver - Updated to new Webdriver version. Changed promise.resolve() to promise.fulfill()

--- test/acceptance/leaderboard.spec.js
+++ test/acceptance/leaderboard.spec.js
@@ -4,75 +4,16 @@
     jasmine.getEnv().defaultTimeoutInterval = 20000;
 
     // GENERIC MIXIN
-    var http = require('http'),
-        request = require('request'),
-        webdriver = require('../rtd/node_modules/selenium-webdriver'),
-        driver,
+    var helper = require('../rtd').helper,
+        webdriver = require('selenium-webdriver'),
         flow = webdriver.promise.controlFlow(),
-        newRun;
+        driver;
 
-    var getWebdriverSessions = function (callback) {
-        request.get({
-            url: 'http://localhost:4444/wd/hub/sessions',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }, function (error, response, body) {
-            callback(JSON.parse(body).value);
-        });
-    };
-
-    var getWebdriverSessionStatus = function (sessionId, callback) {
-        request.get({
-            url: 'http://localhost:4444/wd/hub/session/' + sessionId + '/url',
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }, function (error, response) {
-            callback(response.statusCode);
-        });
-    };
-
-    var deleteWebdriverSession = function (sessionId) {
-        request.del({
-            url: 'http://localhost:4444/wd/hub/session/' + sessionId,
-            headers: {
-                'Accept': 'application/json'
-            }
-        }, null);
-    };
-
-    var deleteWebdriverSessions = function (sessions) {
-        for (var i = 0; i < sessions.length; i += 1) {
-            deleteWebdriverSession(sessions[i].id);
-        }
-    };
-
-    var reuseOrCreateSession = function (sessions) {
-        if (sessions.length === 0) {
-            driver = require('../rtd/webdrivers/selenium-server.js')(webdriver, { browserName: 'chrome' });
-            driver.manage().timeouts().setScriptTimeout(2000);
-            driver.manage().timeouts().implicitlyWait(2000);
-        } else {
-            var tempDriver = require('../rtd/webdrivers/selenium-server.js')(webdriver, { browserName: 'chrome' }, sessions[0].id);
-            getWebdriverSessionStatus(sessions[0].id, function (status) {
-                if (status !== 200) {
-                    deleteWebdriverSessions(sessions);
-                    tempDriver = require('../rtd/webdrivers/selenium-server.js')(webdriver, { browserName: 'chrome' });
-                }
-                tempDriver.manage().timeouts().setScriptTimeout(2000);
-                tempDriver.manage().timeouts().implicitlyWait(2000);
-                driver = tempDriver;
-            });
-        }
-    };
-
-    getWebdriverSessions(reuseOrCreateSession);
 
     var resetApp = function () {
         var deferred = webdriver.promise.defer();
         driver.get('http://localhost:8000/reset').then(function () {
-            deferred.resolve();
+            deferred.fulfill();
         });
         return deferred.promise;
     };
@@ -80,29 +21,9 @@
     var openApp = function () {
         var deferred = webdriver.promise.defer();
         driver.get('http://localhost:8000').then(function () {
-            deferred.resolve();
+            deferred.fulfill();
         });
         return deferred.promise;
-    };
-
-
-    var postBackCoverage = function () {
-        return driver.executeScript(function () {
-            document.postCoverage();
-        });
-    };
-
-    var waitForWebdriver = function (callback) {
-        if (driver) {
-            callback();
-        }
-        newRun = true;
-        waitsFor(function () {
-            return driver;
-        }, "Webdriver did not initialize.\nYou may need to restart RTD", 10000);
-        runs(function () {
-            callback();
-        });
     };
 
     var error = function (err) {
@@ -131,7 +52,7 @@
                 if (value.indexOf(email) !== 0) {
                     deferred.rejected(value + ' did not contain ' + email);
                 } else {
-                    deferred.resolve();
+                    deferred.fulfill();
                 }
             });
         return deferred.promise;
@@ -154,7 +75,7 @@
                             playerName.getText().then(function (value) {
                                 if (value === name) {
                                     resolved = true;
-                                    matchDefer.resolve(player);
+                                    matchDefer.fulfill(player);
                                 }
                             });
                         });
@@ -163,11 +84,10 @@
 
                 flow.execute(function () {
                     matchPromise.then(function (player) {
-                        mainDefer.resolve(player);
+                        mainDefer.fulfill(player);
                     });
                     expect(resolved).toBe(true);
                 });
-
             });
             return mainDefer.promise;
         };
@@ -181,7 +101,7 @@
         var mainDefer = webdriver.promise.defer();
         driver.findElement(webdriver.By.className('inc')).then(function (inc) {
             inc.click();
-            mainDefer.resolve(player);
+            mainDefer.fulfill(player);
         });
         return mainDefer.promise;
     };
@@ -191,7 +111,7 @@
         player.findElement(webdriver.By.className('score')).then(function (playerScore) {
             playerScore.getText().then(function (value) {
                 expect(parseInt(value, 10)).toBe(points);
-                mainDefer.resolve(player);
+                mainDefer.fulfill(player);
             });
         });
         return mainDefer.promise;
@@ -207,7 +127,8 @@
 
     beforeEach(function () {
         var ready = false;
-        waitForWebdriver(function () {
+        helper.getDriverPromise().then(function (result) {
+            driver = result;
             resetApp().
                 then(setupPlayers).
                 then(openApp).
@@ -222,7 +143,7 @@
 
     afterEach(function() {
         var ready = false;
-        postBackCoverage().then(function () {
+        helper.postBackCoverage().then(function () {
             ready = true;
         });
         waitsFor(function () {
